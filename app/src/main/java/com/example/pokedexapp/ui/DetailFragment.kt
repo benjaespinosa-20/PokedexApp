189c08126bfea888ee4b98b29af9ff8687cd104f
benja <benjaespinosa98@gmail.com>
2023-08-15 23:13:40 -0600

    Initial commit and initial configuration

---
+++ app/src/main/java/com/example/pokedexapp/ui/DetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.pokedexapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pokedexapp.R
+import com.example.pokedexapp.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+    private lateinit var binding: FragmentDetailBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailBinding.bind(view)
+    }
+}
